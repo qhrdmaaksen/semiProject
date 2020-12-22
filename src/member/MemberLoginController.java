d5a0a7c6534ba564df31f0ffe9df4e283db0d5fa
young <dud832@gmail.com>
2020-12-22 16:30:35 +0900

    로그인 컨트롤러 코딩
    로그인 컨트롤러 및 다오 코딩했습니다.

--- src/member/MemberLoginController.java
+++ src/member/MemberLoginController.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.MemberDao;
+import VO.MemberVO;
 import common.SuperClass;
 
 public class MemberLoginController extends SuperClass{
@@ -31,7 +33,39 @@ public class MemberLoginController extends SuperClass{
 		this.id = request.getParameter("id");
 		this.password = request.getParameter("password");
 		
+		if(this.validate(request) == false) {
+			String gotopage ;
+			gotopage = "login/login.jsp";
+			
+			String errmsg = "회원 정보가 없습니다.";
+			
+			
+			super.setErrorMessage(errmsg);
+			super.doPost(request, response);
+			super.GotoPage(gotopage);
+			
+			
+		}
 		
+		if(this.validate(request)==true) {
+			
+			MemberDao dao = new MemberDao();
+			
+			MemberVO member =  dao.Insertdate(id,password);
+			
+			super.session.setAttribute("loginfo", member);
+			
+			String gotopage ;
+			gotopage = "common/index.jsp";
+			
+			super.GotoPage(gotopage);
+			
+			
+		}
+		
+		
+		
+	
 		
 		
 		
@@ -40,10 +74,22 @@ public class MemberLoginController extends SuperClass{
 	
 	@Override
 	public boolean validate(HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		return super.validate(request);
+		boolean isCheck = true;	// 기본 값은 true 입니다. 
+		// 만일 유효성 검사의 문제가 있으면 isCheck 는 false 로 변경합니다.
+		
+		if(this.id.length() < 4 || this.id.length()> 10) {
+			request.setAttribute(super.PREFIX+"id", "아이디는 4자리 이상 10자리 이하이어야 합니다.");
+			
+			isCheck = false;
+		}
+		
+		if(this.password.length() < 4 || this.password.length()> 10) {
+			request.setAttribute(super.PREFIX+"passowrd", "비밀번호는 4자리 이상 10자리 이하이어야 합니다.");
+			isCheck = false;
+		}
+		
+		return isCheck;
 	}
-	
 	
 
 }
