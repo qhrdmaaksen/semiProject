02e07939d48e402667549d4887367bf1b7112c5f
kwonyk <dusrud0310@hanmail.net>
2021-01-17 00:08:05 +0900

    정기 상품 구매 탭 적용완료

--- src/Mall/MallCartAddController.java
+++ src/Mall/MallCartAddController.java
@@ -16,67 +16,72 @@ public class MallCartAddController extends SuperClass{
 	int qty = 0 ;
 	int months = 0 ;
 
-@Override
-public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
-	super.doPost(request, response);
-
-	String gotopage = "" ;
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		super.doPost(request, response);
 	
-	if (super.session.getAttribute("loginfo") == null) {
-		// 로그인 하지 않았다면 로그인 페이지로 이동
-		gotopage = "member/login.jsp" ;
-		super.GotoPage(gotopage);
-	} else { // 누군가 로그인 한 상태입니다.
-		System.out.println("하하하");
-		int productcode = Integer.parseInt(request.getParameter("productcode")) ;
-		int stock = Integer.parseInt(request.getParameter("stock")) ; // 재고
-		if (request.getParameter("qty") == null) {
-			this.qty = 0 ;
-		}else {
-			this.qty = Integer.parseInt(request.getParameter("qty")) ; // 구매 수량
-		}
-		if (request.getParameter("months") == null) {
-			months = 0 ;
-		}else {
-			months = Integer.parseInt(request.getParameter("months")) ; // 구매 수량
-		}
+		String gotopage = "" ;
 		
-		System.out.println(productcode + "/" + stock  + "/" + qty + "/" + months);
-		
-		if (stock < qty || stock < months) { // 재고 수량 초과
-			String message = "재고 수량이 부족합니다." ;
-			super.setErrorMessage(message);
-			new PlistController().doGet(request, response);
-			
-		} else { // 판매에 문제 없슴
-			MyCartList mycart = (MyCartList)super.session.getAttribute("mycart") ;
-			MyCartList Rmycart = (MyCartList)super.session.getAttribute("Rmycart") ;
-			
-			
-			 if (mycart == null && Rmycart == null) { 
-				 mycart = new MyCartList() ;
-				 Rmycart = new MyCartList() ; 
-				 }
-			 
-			if (this.qty == 0) {
-				System.out.println("왜 값이 없다고 나오냐고ㅠㅠ" + productcode + "/" + this.months);
-				Rmycart.AddROrder(productcode, this.months); // 카트에 담기
-				super.session.setAttribute("Rmycart", Rmycart);
-			} else if (this.months == 0){
-				System.out.println("왜 값이 없다고 나오냐고ㅠㅠ2" + productcode + "/" + this.qty);
-				mycart.AddOrder(productcode, this.qty); // 카트에 담기
-				super.session.setAttribute("mycart", mycart);
+		if (super.session.getAttribute("loginfo") == null) {
+			// 로그인 하지 않았다면 로그인 페이지로 이동
+			gotopage = "member/login.jsp" ;
+			super.GotoPage(gotopage);
+		} else { // 누군가 로그인 한 상태입니다.
+			System.out.println("하하하");
+			int productcode = Integer.parseInt(request.getParameter("productcode")) ;
+			int stock = Integer.parseInt(request.getParameter("stock")) ; // 재고
+			if (request.getParameter("qty") == null) {
+				this.qty = 0 ;
+			}else {
+				this.qty = Integer.parseInt(request.getParameter("qty")) ; // 구매 수량
 			}
-			//super.session.setAttribute("mycart", mycart);
-			//super.session.setAttribute("Rmycart", Rmycart);
+			if (request.getParameter("months") == null) {
+				months = 0 ;
+			}else {
+				months = Integer.parseInt(request.getParameter("months")) ; // 구매 수량
+			}
 			
-			System.out.println(this.qty + "/" + this.months);
-			System.out.println("어떻게 나오나용"+Rmycart + "/" + mycart);
-			// 장바구니 목록 페이지로 이동합니다.
-			new MallCartListController().doGet(request, response) ;
-		}
-	}		
-}
+			System.out.println(productcode + "/" + stock  + "/" + qty + "/" + months);
+			
+			if (stock < qty || stock < months) { // 재고 수량 초과
+				String message = "재고 수량이 부족합니다." ;
+				super.setErrorMessage(message);
+				new PlistController().doGet(request, response);
+				
+			} else { // 판매에 문제 없슴
+				MyCartList mycart = (MyCartList)super.session.getAttribute("mycart") ;
+				MyCartList Rmycart = (MyCartList)super.session.getAttribute("Rmycart") ;
+				
+				
+				if (mycart == null) {
+					 mycart = new MyCartList() ;
+				} else if(Rmycart == null){
+					Rmycart = new MyCartList() ;
+				}
+				
+				
+				/*
+				 * if (mycart == null && Rmycart == null) { mycart = new MyCartList() ; Rmycart
+				 * = new MyCartList() ; }
+				 */
+				 
+				if (this.qty == 0) {
+					Rmycart.AddROrder(productcode, this.months); // 카트에 담기
+					super.session.setAttribute("Rmycart", Rmycart);
+				} else if (this.months == 0){
+					mycart.AddOrder(productcode, this.qty); // 카트에 담기
+					super.session.setAttribute("mycart", mycart);
+				}
+				//super.session.setAttribute("mycart", mycart);
+				//super.session.setAttribute("Rmycart", Rmycart);
+				
+				System.out.println(this.qty + "/" + this.months);
+				System.out.println("어떻게 나오나용"+Rmycart + "/" + mycart);
+				// 장바구니 목록 페이지로 이동합니다.
+				new MallCartListController().doGet(request, response) ;
+			}
+		}		
+	}
 }
 
 
